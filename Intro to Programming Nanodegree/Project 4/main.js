7e77601479f233626b627ffc54f7b478bbd28a98
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-01-15 17:54:00 -0600

    feat: Implement scrolling animation to selected movie
    Clicking on a movie now correctly scrolls to the movie which is placed directly above the footer.
    Additionally, the footer box has been correctly implemented to place text next to the trailer when there is room or under it otherwise.
    Javascript functions were placed in the $(document).ready() function so that they are not run until the document is ready.

--- Intro to Programming Nanodegree/Project 4/main.js
+++ Intro to Programming Nanodegree/Project 4/main.js
@@ -2,28 +2,36 @@ $(document).ready(function() {
   $("#content").on("click", {movie: null}, display);
   $(".movie").on("click", {movie: "movie"}, display);
   $("#footer").hide();
-});
+  $("#footer").css("visibility", "visible");
 
-function display(event) {
-  event.cancelBubble = true;
-  event.stopPropagation();
-  if (event.data.movie == null) {
-    $("#footer").slideUp(400, setContentHeight(null));
+  function display(event) {
+    event.cancelBubble = true;
+    event.stopPropagation();
+    if (event.data.movie == null) {
+      $("#footer").slideUp(400, setContentHeight(null));
+    }
+    else {
+      $("#footer").slideDown(400, setContentHeight("#footer"));
+      $("html, body").animate({
+        scrollTop: scrollHelper(this)
+      });
+    }
   }
-  else {
-    $("#footer").slideDown(400, setContentHeight("#footer"));
-    $("html, body").animate({
-      scrollTop: $(this).offset().top
+
+  function scrollHelper(element) {
+    //window.alert($(element).next().attr('id'));
+    var currentOffset = $(element).offset().top;
+    var windowOffset = $("#footer").innerHeight() - $(window).height();
+    return currentOffset + windowOffset + $(element).innerHeight();
+  }
+
+  function setContentHeight(element) {
+    var height = 0;
+    if(element != null) {
+      height = $(element).outerHeight();
+    }
+    $("#content").animate({
+      marginBottom: height
     });
   }
-}
-
-function setContentHeight(element) {
-  var height = 0;
-  if(element != null) {
-    height = $(element).outerHeight();
-  }
-  $("#content").animate({
-    marginBottom: height
-  });
-}
+});
