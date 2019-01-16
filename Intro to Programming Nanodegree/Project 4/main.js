7d594d3ee163ed401d4baa41ee8b9d7ef178aa5a
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-01-16 17:52:14 -0600

    refactor: Add documentation, switch css ids to classes, and improve class names
    Documentation added to html, css, and js files.
    All ids used for styling replaced with classes due to Udacity style recommendations.
    'movie' class name changed to 'poster' to better reflect purpose.

--- Intro to Programming Nanodegree/Project 4/main.js
+++ Intro to Programming Nanodegree/Project 4/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  $("#content").on("click", {movie: null}, display);
-  $(".movie").on("click", {movie: "movie"}, display);
-  $("#footer").hide();
-  $("#footer").css("visibility", "visible");
+  $(".content").on("click", {movie: null}, display);
+  $(".poster").on("click", {movie: "movie"}, display);
+  $(".footer").hide();
+  $(".footer").css("visibility", "visible");
 
   /**
-
+  @description Toggle the existance of the footer when a poster is clicked on.
   */
   function display(event) {
     event.cancelBubble = true;
     event.stopPropagation();
     if (event.data.movie === null) {
-      $("#footer").slideUp(setContentHeight(null));
-      $("#trailer").attr("src", "");
+      $(".footer").slideUp(setContentHeight(null));
+      $(".trailer").attr("src", "");
     }
     else {
-      $("#trailer").attr("src", "https://www.youtube.com/embed/E7N7v4qy8zM?controls=0&rel=0&autoplay=1");
-      $("#footer").slideDown(setContentHeight("#footer"));
+      $(".trailer").attr("src", "https://www.youtube.com/embed/E7N7v4qy8zM?controls=0&rel=0&autoplay=1");
+      $(".footer").slideDown(setContentHeight(".footer"));
       $("html, body").animate({
         scrollTop: scrollHelper(this)
       });
@@ -24,24 +24,26 @@ $(document).ready(function() {
   }
 
   /**
-
+  @description Place extra space at the bottom based on the element passed in.
+  @param {Element} element - Element to base the space at the bottom of content on.
   */
   function setContentHeight(element) {
     var height = 0;
-    if(element !== null) {
+    if(element) {
       height = $(element).outerHeight();
     }
-    $("#content").animate({
+    $(".content").animate({
       marginBottom: height
     });
   }
 
   /**
-
+  @description Smoothly scroll to target element. Place it directly above the footer.
+  @param {Element} element - Target element for scrolling.
   */
   function scrollHelper(element) {
     var currentOffset = $(element).offset().top;
-    var windowOffset = $("#footer").innerHeight() - $(window).height();
+    var windowOffset = $(".footer").innerHeight() - $(window).height();
     return currentOffset + windowOffset + $(element).innerHeight();
   }
 });
