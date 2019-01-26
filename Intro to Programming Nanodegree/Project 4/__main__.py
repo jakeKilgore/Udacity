b9c164048ae6b23078df5dd7e12782b45ae747f2
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-01-25 23:55:25 -0600

    feat: Dynamically populate movies list with python script
    The web page is now created and opened by __main__.py.
    Placeholder movies removed from mockup.
    Movie class now generates own html.
    movies.json file holds information on movies to be added to the webpage.

--- Intro to Programming Nanodegree/Project 4/__main__.py
+++ Intro to Programming Nanodegree/Project 4/__main__.py
@@ -1,24 +1,42 @@
+import json
 import webbrowser
 import classes.media as media
-poster_html = '''
-<div class="poster-container">
-    <img class="poster" 
-        src="{poster-url}"
-        data-trailer="{trailer-url}"
-        data-title="{title}"
-        data-description="{description}"
-    >
-</div>
-<h2 class="poster-title">{title}</h2>
-            '''
+
 
 def main():
-    open_webpage()
+    movies = get_movies_list()
+    webpage = generate_webpage(movies)
+    open_webpage(webpage)
 
 
-def open_webpage():
-    movie = media.Movie("", "", "", "")
-    webbrowser.open("mockup.html")
+def get_movies_list():
+    with open('movies.json') as data:
+        movie_data = json.load(data)
+    movies = []
+    for movie in movie_data:
+        movies.append(media.Movie(movie['title'], movie['description'], movie['trailer'], movie['poster']))
+    return movies
+
+
+def generate_webpage(movies):
+    movies_html = generate_movies_html(movies)
+    return fill_template(movies_html)
+
+
+def generate_movies_html(movies):
+    movies_html = [movie.html() for movie in movies]
+    return ''.join(movies_html)
+
+
+def fill_template(movies_html):
+    with open("mockup.html", "r") as template:
+        return template.read().format(movies=movies_html)
+
+
+def open_webpage(webpage):
+    with open("output.html", "w") as output:
+        output.write(webpage)
+    webbrowser.open("output.html")
 
 
 if __name__ == "__main__":
--- Intro to Programming Nanodegree/Project 4/classes/media.py
+++ Intro to Programming Nanodegree/Project 4/classes/media.py
@@ -1,6 +1,24 @@
+poster_html = '''
+        <div class="movie">
+            <div class="poster-container">
+                <img class="poster" 
+                    src="{poster_url}"
+                    data-trailer="{trailer_url}"
+                    data-title="{title}"
+                    data-description="{description}"
+                />
+            </div>
+            <h2 class="poster-title">{title}</h2>
+        </div>
+            '''
+
 class Movie:
     def __init__(self, title, description, trailer, poster):
         self.title = title
         self.description = description
         self.trailer = trailer
         self.poster = poster
+
+    def html(self):
+        return poster_html.format(poster_url=self.poster, trailer_url=self.trailer,
+                                  title=self.title, description=self.description)
