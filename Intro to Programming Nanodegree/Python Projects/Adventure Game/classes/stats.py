1f0d91473b557468264ae490ffda37b1fbd5729b
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-04-20 01:51:29 -0500

    feat: Changed Stats class from integers instantiated by an array to a collection of Stat objects made up of a value and a modifier.
    The use of the stats class was unclear from the outside. Now getting the value and the modifier of the stat are explicit.

--- Intro to Programming Nanodegree/Python Projects/Adventure Game/classes/stats.py
+++ Intro to Programming Nanodegree/Python Projects/Adventure Game/classes/stats.py
@@ -2,15 +2,51 @@
 
 
 class Stats:
-    def __init__(self, stat_array):
-        assert len(stat_array) == 6
-        self.strength = stat_array[0]
-        self.dexterity = stat_array[1]
-        self.constitution = stat_array[2]
-        self.intelligence = stat_array[3]
-        self.wisdom = stat_array[4]
-        self.charisma = stat_array[5]
+    """Class handling the ability scores of actors. Ability scores govern how good the actor is at related skills.
 
-    @staticmethod
-    def modifier(value):
-        return int(value / 2) - 5
+    Ability scores range between 1 and 30 and give a bonus modifier equal to (score - 10) / 2 to associated skills.
+    By default, all stats are 10.
+
+    Attributes:
+        strength (int): Measures the actor's bodily power and athletic training.
+        dexterity (int): Measures the actor's agility, reflexes, and balance.
+        constitution (int): Measures the actor's health, stamina, and vital force.
+        intelligence (int): Measures the actor's mental acuity, accuracy of recall, and ability to reason.
+        wisdom (int): Measures the actor's attunement to the world around them, perceptiveness, and intuition.
+        charisma (int): Measures the actor's ability to interact effectively with others.
+    """
+    def __init__(self, strength=10, dexterity=10, constitution=10, intelligence=10, wisdom=10, charisma=10):
+        """Constructor for the Stats class.
+
+        Parameters:
+            strength (int): The value of the actor's strength ability score.
+            dexterity (int): The value of the actor's dexterity ability score.
+            constitution (int): The value of the actor's constitution ability score.
+            intelligence (int): The value of the actor's intelligence ability score.
+            wisdom (int): The value of the actor's wisdom ability score.
+            charisma (int): The value of the actor's charisma ability score.
+        """
+        self.strength = Stat(strength)
+        self.dexterity = Stat(dexterity)
+        self.constitution = Stat(constitution)
+        self.intelligence = Stat(intelligence)
+        self.wisdom = Stat(wisdom)
+        self.charisma = Stat(charisma)
+
+
+class Stat:
+    """Class handling the individual ability scores of the actor.
+
+    Attributes:
+        value (int): The value tied to the ability score.
+        modifier (int): The bonus applied to skills tied to the ability score.
+    """
+    def __init__(self, value):
+        """Constructor for the Stat class.
+
+        Parameters:
+            value (int): The value tied to the ability score.
+        """
+        self.value = value
+        self.modifier = int((value - 10) / 2)
+
