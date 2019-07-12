2a910a1fc5b125769122a69a5d86f4c479348122
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-07-12 13:09:36 -0500

    docs: Add documentation to party module

--- Intro to Programming Nanodegree/Python Projects/Adventure Game/classes/party.py
+++ Intro to Programming Nanodegree/Python Projects/Adventure Game/classes/party.py
@@ -1,19 +1,35 @@
 # -*- coding: UTF-8
 from collections.abc import Mapping
-from ..classes.actors.actor import Actor
+from .actors.actor import Actor
 
 
 class Party(Mapping):
+    """Class for handling groups of actors working together.
+
+    Attributes:
+        members (Dictionary[Actor]): A group of actors working together.
+    """
     def __init__(self, members):
+        """Constructor for the Party class.
+
+        Parameters:
+            members (List[Actor]): A group of actors working together.
+        """
         self.members = {}
         for member in members:
             self.members[member.name] = member
 
     def __iter__(self):
+        """Iterate through the members of the party."""
         for member in self.members.values():
             yield member
 
     def __getitem__(self, member):
+        """Retrieve one of the members of the party, if it is present.
+
+        Parameters:
+            member (Actor or str): The member to check for. This may be the name or the Actor object of the member.
+        """
         if isinstance(member, Actor):
             key = member.name
         else:
@@ -21,15 +37,24 @@ class Party(Mapping):
         return self.members.__getitem__(key)
 
     def __len__(self):
+        """The number of members in the party."""
         return self.members.__len__()
 
     def __str__(self):
+        """String representation of the party."""
         string = ""
         for name in self.members:
             string += f"{name} "
         return string
 
     def discard(self, member):
+        """Remove a member from the party, if it is present.
+
+        If the given parameter is not in the party, no error is raised.
+
+        Parameters:
+            member (Actor or str): The member to remove. This may be the name or the Actor object of the member.
+        """
         if isinstance(member, Actor):
             key = member.name
         else:
