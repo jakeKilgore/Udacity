5e75aaadc19a3b33e53e3fd740c2141f1a50674f
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-07-12 13:08:58 -0500

    refactor: attempt to make main file part of path so that it will run through the console

--- Intro to Programming Nanodegree/Python Projects/Adventure Game/__main__.py
+++ Intro to Programming Nanodegree/Python Projects/Adventure Game/__main__.py
@@ -1,26 +1,29 @@
 # -*- coding: UTF-8
-from classes.actors.bandit import Bandit
-from classes.actors.player import Player
-from classes.combat import Combat
-from classes.party import Party
+import classes.actors.bandit as bandit
+import classes.actors.player as player
+import classes.combat as combat
+import classes.party as party
 
 
 def main():
-    main_character = Player()
+    main_character = player.Player()
     enemy = random_enemy()
     story(main_character, enemy)
     return
 
 
 def story(main_character, enemy):
-    combat = Combat(Party([main_character]), Party([enemy]), False)
-    print(combat.combat_order)
-    combat.engage()
+    fight = combat.Combat(party.Party([main_character]), party.Party([enemy]), False)
+    fight.engage()
 
 
 def random_enemy():
-    return Bandit()
+    return bandit.Bandit()
 
 
 if __name__ == "__main__":
+    import os
+    import sys
+    path = os.path.dirname(os.path.join(os.getcwd(), __file__))
+    sys.path.append(os.path.normpath(os.path.join(path, '..', '..')))
     main()
