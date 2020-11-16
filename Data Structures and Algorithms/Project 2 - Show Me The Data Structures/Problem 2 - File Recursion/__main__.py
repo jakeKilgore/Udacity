3240b99e9e00f27b1a0c046a9356262a314601c7
Jacob Kilgore <jake.h.kilgore@gmail.com>
2020-11-16 13:52:30 -0600

    Implemented Problem 2 - File Recursion in DS&A.

---
+++ Data Structures and Algorithms/Project 2 - Show Me The Data Structures/Problem 2 - File Recursion/__main__.py
@@ -0,0 +1,34 @@
+import os
+
+def main():
+    path = os.path.dirname(os.path.realpath(__file__))
+    print(find_files('.c', path))
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    paths = []
+    elements = [os.path.join(path, element) for element in os.listdir(path)]
+    for element in elements:
+        if os.path.isdir(element):
+            paths += find_files(suffix, element)
+        elif os.path.isfile(element):
+            if element.endswith(suffix):
+                paths.append(element)
+    return paths
+
+if __name__ == "__main__":
+    main()
