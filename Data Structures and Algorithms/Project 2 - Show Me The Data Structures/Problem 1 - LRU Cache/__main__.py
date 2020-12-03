431ed684dfad8d0cc1513ad869cae5cf6259bc16
Jacob Kilgore <jake.h.kilgore@gmail.com>
2020-12-03 17:50:33 -0600

    Removed unnecessary whitespace.

--- Data Structures and Algorithms/Project 2 - Show Me The Data Structures/Problem 1 - LRU Cache/__main__.py
+++ Data Structures and Algorithms/Project 2 - Show Me The Data Structures/Problem 1 - LRU Cache/__main__.py
@@ -7,12 +7,12 @@ def main():
     our_cache.set(2, 2)
     our_cache.set(3, 3)
     our_cache.set(4, 4)
-    
+
     assert(our_cache.get(1) == 1)       # returns 1
     assert(our_cache.get(2) == 2)       # returns 2
     assert(our_cache.get(9) == -1)      # returns -1 because 9 is not present in the cache
 
-    our_cache.set(5, 5) 
+    our_cache.set(5, 5)
     our_cache.set(6, 6)
 
     assert(our_cache.get(3) == -1)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
