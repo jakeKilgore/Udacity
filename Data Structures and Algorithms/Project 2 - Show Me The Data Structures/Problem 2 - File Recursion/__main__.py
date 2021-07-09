0e5b9ffbdefa32744e480117c583e6eec1fe3a38
Jacob Kilgore <jake.h.kilgore@gmail.com>
2021-07-09 16:35:13 -0400

    docs: Updated documentation for File Recursion project.

--- Data Structures and Algorithms/Project 2 - Show Me The Data Structures/Problem 2 - File Recursion/__main__.py
+++ Data Structures and Algorithms/Project 2 - Show Me The Data Structures/Problem 2 - File Recursion/__main__.py
@@ -4,21 +4,19 @@ def main():
     path = os.path.dirname(os.path.realpath(__file__))
     print(find_files('.c', path))
 
-def find_files(suffix, path):
-    """
-    Find all files beneath path with file name suffix.
+def find_files(suffix: str, path: str) -> list[str]:
+    """Find all files beneath path with file name suffix.
 
     Note that a path may contain further subdirectories
     and those subdirectories may also contain further subdirectories.
-
     There are no limit to the depth of the subdirectories can be.
 
-    Args:
-      suffix(str): suffix if the file name to be found
-      path(str): path of the file system
+    Parameters:
+      suffix (str): suffix if the file name to be found
+      path (str): path of the file system
 
     Returns:
-       a list of paths
+       list[str]: a list of paths
     """
     paths = []
     elements = [os.path.join(path, element) for element in os.listdir(path)]
